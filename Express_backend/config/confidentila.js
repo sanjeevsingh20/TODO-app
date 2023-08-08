66b1d9b16d270fe9df39c9726bbe708bec5bf045
Sanjeev singh <sk2747267@gmail.com>
2023-08-08 22:20:30 +0530

    Update confidentila.js

--- Express_backend/config/confidentila.js
+++ Express_backend/config/confidentila.js
@@ -1,8 +1,8 @@
-const emai_password = "ryjkmvqagolvmrkd";
-const email_user = "manojkumarsinghy20@gmail.com";
+const emai_password = "";
+const email_user = "yourmail@gmail.com";
 const secret_key = "yametekhudasai";
-const data_password = "@#Sanjukumar123";
-const uri ="mongodb+srv://sanjubabavines:Sanjeevkhush12@cluster0.fpc9xh3.mongodb.net/Notebook?retryWrites=true&w=majority"
+const data_password = "";
+const uri ="mongodb+srv://<usernamr>:<password>2@cluster0.fpc9xh3.mongodb.net/Notebook?retryWrites=true&w=majority"
 
 module.exports = {
   emai_password,
